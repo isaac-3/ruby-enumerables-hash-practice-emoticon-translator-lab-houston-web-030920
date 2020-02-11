ad338dd525de765cec55de1f518a4b25ff38c1ef
isaac c <isaacgc0596@gmail.com>
2020-02-11 05:10:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   emo = load_library(file_path)
-  
+    binding.pry
   jap_emo = emo["get_emoticon"][emoticon]
 
   if !jap_emo.include?(jap_emo) 
