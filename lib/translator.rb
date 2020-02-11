9029a578bc15b835046cf69bbddd523554e6529d
isaac c <isaacgc0596@gmail.com>
2020-02-11 05:36:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ def get_japanese_emoticon(file_path, emoticon)
   emo = load_library(file_path)
     binding.pry
   jap_emo = emo["get_emoticon"][emoticon]
-  if
+  if varName
     "Sorry, that emoticon was not found"
   end
   jap_emo
