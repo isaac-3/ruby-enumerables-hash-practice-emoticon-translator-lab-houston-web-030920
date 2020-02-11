09629ef758ce1e41f0c0bf5cbddf8ee9907e2649
isaac c <isaacgc0596@gmail.com>
2020-02-11 06:00:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,6 +23,12 @@ def get_japanese_emoticon(file_path, emoticon)
   end
 end 
 
-def get_english_meaning
+def get_english_meaning(file_path, emoticon)
+  emo = load_library(file_path)
+  
+  
+  
+  
+
   # code goes here
 end
