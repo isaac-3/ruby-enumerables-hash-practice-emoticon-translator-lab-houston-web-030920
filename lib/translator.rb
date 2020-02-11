7da263b83eb0fb66f044485178c4f8ff4b909341
isaac c <isaacgc0596@gmail.com>
2020-02-11 06:03:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,6 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   emo = load_library(file_path)
-    # binding.pry
   jap_emo = emo["get_emoticon"][emoticon]
   if jap_emo
     jap_emo
@@ -25,6 +24,7 @@ end
 
 def get_english_meaning(file_path, emoticon)
   emo = load_library(file_path)
+  eng_meaning = emo["get_meaning"][emoticon]
   
   
   
@@ -32,3 +32,7 @@ def get_english_meaning(file_path, emoticon)
 
   # code goes here
 end
+
+
+
+
