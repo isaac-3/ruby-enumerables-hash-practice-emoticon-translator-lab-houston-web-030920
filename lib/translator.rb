67ebb68ca29b6b9d5a905731bc76af412350f0f8
isaac c <isaacgc0596@gmail.com>
2020-02-11 04:58:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,12 +1,25 @@
 # require modules here
+require "yaml"
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  files = YAML.load_file(file_path)
+  result = {"get_meaning" => {}, "get_emoticon" => {}}
+  files.each do |meaning, emoticon|
+    result["get_meaning"][emoticon[1]] = meaning
+    result["get_emoticon"][emoticon[0]] = emoticon[1]
+  end
+  result
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
+def get_japanese_emoticon(file_path, emoticon)
+  emo = load_library(file_path)
+  jap_emo = emo["get_emoticon"][emoticon]
+  if !jap_emo.include?(jap_emo) 
+    "Sorry, that emoticon was not found"
+  end
+  jap_emo
+end 
 
 def get_english_meaning
   # code goes here
